c7255dc05d419d0b671ac41b4e45b15a5217d61e
Angie Ochoa <ochoaangie014@gmail.com>
2025-06-05 19:15:12 -0400

    added floor detection to the player node

--- scenes/message.gd
+++ scenes/message.gd
@@ -5,6 +5,6 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("volar") and get_tree().paused:
 		get_tree().paused = false
 		get_tree().reload_current_scene()
-		$".".hide()
 	else:
 		$".".hide()
+		$game_over.hide()
